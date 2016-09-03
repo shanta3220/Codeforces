6f3c17ccf49f8d716a5d8c8e5b48c0710f85b4ba
shanta <evilevil4991@yahoo.com>
2016-09-04 00:12:53 +0600

    Create A. Theatre Square.cpp

---
+++ A. Theatre Square.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main ()
+{
+    long long n,m,n;
+    cin >> n >> m >> a;
+    cout << ((n + a - 1) / a) * ((m + a - 1) / a) << "\n"; 
+ 
+    return 0;
+}
+ 
