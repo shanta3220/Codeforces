f10b85e2bd9ebcfe821f686c77688fa3a00bc5c9
shanta <evilevil4991@yahoo.com>
2016-09-23 02:56:30 +0600

    Update 630R - Game.cpp

--- 630R - Game.cpp
+++ 630R - Game.cpp
@@ -5,11 +5,10 @@ using namespace std;
 int main()
 {
     long long n;
-    while(cin >> n && n != EOF){
-        if(n % 2 == 0)
+    cin >> n ;
+    if(n % 2 == 0)
         cout << "2\n";
-        else cout << "1\n";
+    else cout << "1\n";
         
-    }
     return 0;
 }
